58ca8c31069edf2573163034e3de6e3934ab52dc
lixuanqi <lixuanqi1995@gmail.com>
2018-06-02 21:47:34 +0800

    409

---
+++ 409-longest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), 0);
+            }
+            if (map.get(s.charAt(i)) == 1) {
+                map.put(s.charAt(i), 0);
+                count = count + 2;
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        if (map.containsValue(1)) {
+            count++;
+        }
+        return count;     
+    }
+}
