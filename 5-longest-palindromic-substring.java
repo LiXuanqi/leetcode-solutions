224da9d181947964c6b506c48b7f1b02c828a1bc
lixuanqi <lixuanqi1995@gmail.com>
2018-06-15 20:07:08 +0800

    5

--- 5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring.java
@@ -1,3 +1,45 @@
+// central axis
+class Solution {
+  public String longestPalindrome(String s) {
+      // [1, 2]
+      // [1, 2, 3]
+      int start = 0, len = 0, longest = 0;
+      for (int i = 0; i < s.length(); i++) {
+          // central axis is on the number.
+          // "1 2 1 4"
+          //    l
+          //    r  len = 3
+          len = isPalindrome(s, i, i);
+          if (len > longest) {
+              longest = len;
+              start = i - len / 2;
+          }
+          // central axis is between two numbers.
+          // "1 2 2 1 4"
+          //    l r    len = 4
+          len = isPalindrome(s, i, i + 1);
+          if (len > longest) {
+              longest = len;
+              start = i - len / 2 + 1;
+          }         
+      }
+      return s.substring(start, start + longest);
+  }
+  private int isPalindrome (String str, int left, int right) {
+      int len = 0;
+      while (left >= 0 && right < str.length()) {
+          if (str.charAt(left) != str.charAt(right)) {
+              break;
+          }
+          len += left == right ? 1 : 2;
+          left--;
+          right++;
+      }
+      return len;
+  }
+}
+
+// Brute-force
 class Solution {
   public String longestPalindrome(String s) {
       String longest = "";
