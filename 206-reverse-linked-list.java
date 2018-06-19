b40733a038f9b379a5e8e367763d36d1c82ecd68
lixuanqi <lixuanqi1995@gmail.com>
2018-06-19 20:05:52 +0800

    206 fix bug

---
+++ 206-reverse-linked-list.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = null;
+        ListNode current = head;
+        //  null     1      2      3      4       5       null
+        //  prev     curr    next
+        //                                       prev     curr    next
+        // why current != null not current.next != null.
+        // it's ok, but note: now, the current.next = null, you should also make current.next = prev;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
