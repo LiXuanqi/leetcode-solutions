f87709c3b27be50f86971592e5af298eea36b83c
lixuanqi <lixuanqi1995@gmail.com>
2018-06-20 17:23:17 +0800

    206 recursion

--- 206-reverse-linked-list.java
+++ 206-reverse-linked-list.java
@@ -27,3 +27,21 @@ class Solution {
         return prev;
     }
 }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+
+//   1  ->  (  2   ->    3  ->    4  ->    5 )
+// curr       next
+
+//   1  ->  (  2   ->    3  ->    4  ->    5 )
+//                                        curr       next
