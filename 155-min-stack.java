c12382752ce053b9a4acf527c6a1591b51bba15e
lixuanqi <lixuanqi1995@gmail.com>
2018-06-20 16:18:23 +0800

    155 follow up

--- 155-min-stack.java
+++ 155-min-stack.java
@@ -33,3 +33,57 @@ class MinStack {
 
 // Stack1 || 2 1 2 2 1 4 4                
 // Stack2 || 2 1 1 1 1 1 1 
+
+class MinStack {
+  class Pair {
+      public final int left;
+      public final int right;
+      public Pair(int left, int right) {
+          this.left = left;
+          this.right = right;
+      }
+  }
+  
+  Stack<Integer> stack1;
+  Stack<Pair> stack2;
+  /** initialize your data structure here. */
+  public MinStack() {
+      stack1 = new Stack<>();
+      stack2 = new Stack<>();
+  }
+  
+  public void push(int x) {
+      stack1.push(x);
+      if (stack2.empty()) {
+          stack2.push(new Pair(x, stack1.size() - 1));
+      } else {
+          Pair pair = stack2.peek();
+          if (pair.left > x) {
+              stack2.push(new Pair(x, stack1.size() - 1));
+          }
+      }
+  }
+  
+  public void pop() {
+      Pair pair = stack2.peek();    
+      if (pair.left == stack1.peek() && pair.right == stack1.size() - 1) {
+      
+          stack2.pop();
+      }
+      stack1.pop();
+      
+  }
+  
+  public int top() {
+      return stack1.peek();
+  }
+  
+  public int getMin() {
+      return stack2.peek().left;
+  }
+}
+
+// Follow up: how to optimize the space usage of stack2 assumpting there are a lot of duplicate element?
+
+// Stack1 ||   2       1    2 2 1 4 4                
+// Stack2 || <2,0>   <1,1>
