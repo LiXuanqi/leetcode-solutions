f2b3179de995b07cdb01c2433e1038688a867831
lixuanqi <lixuanqi1995@gmail.com>
2018-06-27 21:44:34 +0800

    637

---
+++ 637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Double> averageOfLevels(TreeNode root) {
+      List<Double> result = new ArrayList<>();
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(root);
+      while (!queue.isEmpty()) {
+          int size = queue.size();
+          double sum = 0;
+          for (int i = 0; i < size; i++) {
+              TreeNode node = queue.poll();
+              sum += node.val;
+              if (node.left != null) {
+                  queue.offer(node.left);
+              }
+              if (node.right != null) {
+                  queue.offer(node.right);
+              }
+          }
+          result.add(sum / size); 
+      }
+      return result;
+  }
+}
