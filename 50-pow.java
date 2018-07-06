47d3e302313009a3fc3f9bc4fc4725b7a3f5ff08
lixuanqi <lixuanqi1995@gmail.com>
2018-07-06 16:17:27 +0800

    50

--- 50-pow.java
+++ 50-pow.java
@@ -1,20 +1,27 @@
 class Solution {
-  public double myPow(double x, int n) {
-      if (n >= 0) {
-          return pow(x, n);
-      } else {
-          return 1 / pow(x, Math.abs(n));
-      }
-  }
-  private double pow(double x, int n) {
-      if (n == 0) {
-          return 1;
-      }
-      double half = pow(x, n / 2);
-      if (n % 2 == 0) {
-          return half * half;
-      } else {
-          return half * half * x;
-      }
-  }
+    public double myPow(double x, int n) {
+        if (x == 0 && n == 0) {
+            return 0;
+        }
+        if (n < 0) {
+            return 1 / pow(x, -n);
+        }
+        return pow(x, n);
+    }
+    private double pow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        double power = pow(x, n / 2);
+        if (n % 2 == 0) {
+            return power * power;
+        }
+        return x * power * power;
+    }
 }
+
+// Time: log(n)
+// Space: log(n)
