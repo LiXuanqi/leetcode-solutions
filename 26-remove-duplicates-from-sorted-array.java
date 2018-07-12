4d3bf9dd80714c126cb2be771f74ef555f8b65d4
lixuanqi <lixuanqi1995@gmail.com>
2018-07-12 13:49:49 +0800

    26

--- 26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array.java
@@ -1,11 +1,21 @@
 class Solution {
-  public int removeDuplicates(int[] nums) {
-      int i = 0;
-      for (int j = 0; j < nums.length; j++) {
-          if (nums[i] != nums[j]) {
-              nums[++i] = nums[j];
-          }
-      }
-      return i + 1;
-  }
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        int i = 1;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i - 1] != nums[j]) {
+                nums[i++] = nums[j];
+            }
+            j++;
+        }
+        return i;
+    }
 }
+// Time = O(n)
+// Space = O(1)
