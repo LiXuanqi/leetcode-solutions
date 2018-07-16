6d24d740421165e36a03128e6fbdcdc9608253b3
lixuanqi <lixuanqi1995@gmail.com>
2018-07-16 21:05:49 +0800

    217 better code.

--- 217-contains-duplicate.java
+++ 217-contains-duplicate.java
@@ -1,17 +1,16 @@
 class Solution {
-  public boolean containsDuplicate(int[] nums) {
-      if (nums == null || nums.length == 0) {
-          return false;
-      }
-      Set<Integer> set = new HashSet<>();
-      for (int num : nums) {
-          if (set.contains(num)) {
-              return true;
-          }
-          set.add(num);
-      }
-      return false;
-  }
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 // Time = O(n)
