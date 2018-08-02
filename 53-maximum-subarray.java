b6c397249f5d59b0b2455ca2f66182f89c60898b
lixuanqi <lixuanqi1995@gmail.com>
2018-08-03 01:25:11 +0800

    53 optimize space.

--- 53-maximum-subarray.java
+++ 53-maximum-subarray.java
@@ -3,16 +3,15 @@ class Solution {
       if (nums == null || nums.length == 0) {
           return 0;
       }
-      int[] dp = new int[nums.length];
-      dp[0] = nums[0];
-      int max = dp[0];
+      int dp = nums[0];
+      int max = nums[0];
       for (int i = 1; i < nums.length; i++) {
-          if (dp[i - 1] < 0) {
-              dp[i] = nums[i];
+          if (dp < 0) {
+              dp = nums[i];
           } else {
-              dp[i] = dp[i - 1] + nums[i];
+              dp = dp + nums[i];
           }
-          max = Math.max(max, dp[i]);
+          max = Math.max(max, dp);
       }
       return max;
   }
