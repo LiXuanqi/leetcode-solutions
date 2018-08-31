8cd8654e302007da969bb3b6ea5607364d4ba523
李轩琦 <lixuanqi@lixuanqideMacBook-Pro.local>
2018-08-30 22:04:16 -0700

    36

---
+++ 36-valid-sudoku.java
@@ -0,0 +1,62 @@
+class Solution {
+    class Point {
+        int x;
+        int y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public boolean isValidSudoku(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        for (int i = 0; i < 9; i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (set.contains(board[i][j])) {
+                    return false;
+                }
+                set.add(board[i][j]);
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                 if (board[j][i] == '.') {
+                    continue;
+                }
+                if (set.contains(board[j][i])) {
+                    return false;
+                }
+                set.add(board[j][i]);
+            }
+        }
+        for (int i = 0; i < 9; i = i + 3) {
+            for (int j = 0; j < 9; j = j + 3) {
+                if (!checkCell(new Point(i, j), board)) {
+                    return false;
+                }   
+            }
+        }
+        return true;
+    }
+    private boolean checkCell(Point startIndex, char[][] board) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                 if (board[startIndex.x + i][startIndex.y + j] == '.') {
+                    continue;
+                }
+                if (set.contains(board[startIndex.x + i][startIndex.y + j])) {
+                    return false;
+                }
+                set.add(board[startIndex.x + i][startIndex.y + j]);
+            }
+        }
+        return true;
+    }
+}
