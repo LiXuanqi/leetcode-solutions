25012362c266f4dbea317fe51d54492c1a361e9e
lixuanqi <lixuanqi1995@gmail.com>
2018-09-09 13:10:54 -0700

    138

---
+++ 138-copy-list-with-random-pointer.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return null;
+        }
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        // copy all nodes.
+        RandomListNode curr = head;
+        while (curr != null) {
+            map.put(curr, new RandomListNode(curr.label));
+            curr = curr.next;
+        }
+        // next, random pointers
+        curr = head;
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+        return map.get(head);
+    }
+}
