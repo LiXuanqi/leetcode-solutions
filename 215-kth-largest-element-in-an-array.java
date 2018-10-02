b0affd433de4134d750c86e2e2c73530d9a45f47
lixuanqi <lixuanqi1995@gmail.com>
2018-10-02 00:20:28 -0700

    215

--- 215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array.java
@@ -1,12 +1,47 @@
 class Solution {
-  public int findKthLargest(int[] nums, int k) {
-      Arrays.sort(nums);
-      return nums[nums.length - k];
-  }
-}
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0 || k > nums.length) {
+            return -1;
+        }     
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+    private int quickSelect(int[] nums, int start, int end, int k) {
 
-// sort: O(nlogn)
-// heapify: O(n + k)
-// max heap: O((n + k)logn)
-// min heap: O(nlogk)
-// quick selection: 
+        if (start == end) {
+            return nums[start];
+        }
+        
+        int left = start;
+        int right = end;
+        int mid = left + (right - left) / 2;
+        int pivot = nums[mid];
+        while (left <= right) {
+            while (left <= right && nums[left] > pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] < pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        if (start + k - 1 <= right) {
+            return quickSelect(nums, start, right, k);
+        }
+        if (start + k - 1 >= left) {
+            // why not quickSelect(nums.left, end, k - (left - start));
+            return quickSelect(nums, left, end, k - (left - start));
+        }
+        return nums[right + 1];
+        
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
