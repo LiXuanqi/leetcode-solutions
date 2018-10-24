da6af27540730b34c0441683a4e55a0e2a2692ab
lixuanqi <lixuanqi1995@gmail.com>
2018-10-23 17:05:04 -0700

    22

--- 22-generate-parentheses.java
+++ 22-generate-parentheses.java
@@ -1,30 +1,39 @@
 class Solution {
-  public List<String> generateParenthesis(int n) {
-      List<String> result = new ArrayList<>();
-      if (n == 0) {
-          result.add("");
-          return result;
-      }   
-      dfs(n, 0, 0, new StringBuilder(), result);
-      return result;
-  }
-  private void dfs(int n, int left, int right, StringBuilder temp, List<String> solutions) {
-      if (left + right == 2 * n) {
-          solutions.add(temp.toString());
-          return;
-      }
-      if (left < n) {         
-          temp.append('(');
-          dfs(n, left + 1, right, temp, solutions);
-          temp.deleteCharAt(temp.length() - 1);
-      }
-      if (right < left) {
-          temp.append(')');
-          dfs(n, left, right + 1, temp, solutions);
-          temp.deleteCharAt(temp.length() - 1);
-      }     
-  }
+    public List<String> generateParenthesis(int n) {
+        List<String> solutions = new ArrayList<>();
+        if (n < 0) {
+            return solutions;
+        }
+        if (n == 0) {
+            solutions.add("");
+            return solutions;
+        }
+        dfs(new StringBuilder(), 0, 0, n, solutions);
+        return solutions;
+    }
+    private void dfs(
+        StringBuilder sb,
+        int left,
+        int right,
+        int n,
+        List<String> solutions
+    ) {
+        if (left + right > 2 * n) {
+            return;
+        }
+        
+        if (left == right && left + right == 2 * n) {
+            solutions.add(sb.toString());
+            return;
+        }
+        if (left < right) {
+            return;
+        }
+        sb.append('(');
+        dfs(sb, left + 1, right, n, solutions);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(')');
+        dfs(sb, left, right + 1, n, solutions);
+        sb.deleteCharAt(sb.length() - 1);
+    }
 }
-
-// Space = O(2n)
-// Time = O(2^(2n))
