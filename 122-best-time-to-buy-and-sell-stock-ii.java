9567072964a0087bdd030e7513941fefc2f4bb38
lixuanqi <lixuanqi1995@gmail.com>
2018-10-26 15:13:14 -0700

    122

--- 122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii.java
@@ -3,29 +3,12 @@ class Solution {
         if (prices == null || prices.length == 0) {
             return 0;
         }
-        int buy = 0;
-        int sell = 0;
-        boolean isHandEmpty = true;
+        int profit = 0;
         for (int i = 0; i < prices.length - 1; i++) {
-            // buy
-            if (prices[i] < prices[i + 1]) {
-                if (isHandEmpty) {
-                    buy += prices[i];
-                    isHandEmpty = !isHandEmpty;
-                    System.out.println("buy" + i + " " + buy);
-                    
-                }
-            } else { // sell
-                if (!isHandEmpty) {
-                    sell += prices[i];
-                    isHandEmpty = !isHandEmpty;
-                    System.out.println("sell" + i + " " + sell);
-                }
+            if (prices[i + 1] >= prices[i]) {
+                profit += prices[i + 1] - prices[i];
             }
         }
-        if (!isHandEmpty) {
-            sell += prices[prices.length - 1];
-        }
-        return sell - buy;
+        return profit;
     }
 }
