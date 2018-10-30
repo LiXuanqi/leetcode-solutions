b264591597e6f6f860de8c7ac4e680b293d74ed1
lixuanqi <lixuanqi1995@gmail.com>
2018-10-29 20:20:35 -0700

    332

--- 332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary.java
@@ -1,3 +1,4 @@
+// Method 1: DFS
 class Solution {
     public List<String> findItinerary(String[][] tickets) {
         if (tickets == null || tickets.length == 0 || tickets[0].length != 2) {
@@ -43,6 +44,57 @@ class Solution {
         }
         for (List<String> list : graph.values()) {
             Collections.sort(list);
+        }
+        return graph;
+    }
+}
+
+// Method 2: hierholzer
+class Solution {
+    class Edge {
+        String to;
+        boolean used;
+        public Edge(String to) {
+            this.to = to;
+        }
+    }
+    private List<String> path = new LinkedList<>();
+    public List<String> findItinerary(String[][] tickets) {
+        if (tickets == null || tickets.length == 0 || tickets[0].length != 2) {
+            return null;
+        }
+        Map<String, List<Edge>> graph = buildGraph(tickets);
+        visit("JFK", graph);
+        return path;
+    }
+    private void visit(String start, Map<String, List<Edge>> graph) {
+        if (!graph.containsKey(start)) {
+            path.add(0, start);
+            return;
+        }
+        for (Edge edge : graph.get(start)) {
+            if (edge.used) {
+                continue;
+            }
+            edge.used = true;
+            visit(edge.to, graph);
+        }
+        path.add(0, start);
+    }
+    private Map<String, List<Edge>> buildGraph(String[][] tickets) {
+        Map<String, List<Edge>> graph = new HashMap<>();
+        for (String[] ticket : tickets) {
+            String from = ticket[0];
+            String to = ticket[1];
+            if (!graph.containsKey(from)) {
+                graph.put(from, new ArrayList<>());
+            }
+            graph.get(from).add(new Edge(to));
+        }
+        for (List<Edge> list : graph.values()) {
+            Collections.sort(list, (e1, e2) -> {
+                return e1.to.compareTo(e2.to);
+            });
         }
         return graph;
     }
