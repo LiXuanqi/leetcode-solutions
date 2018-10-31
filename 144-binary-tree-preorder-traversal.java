39591903865a690d18fb721aeff0af60511e2334
lixuanqi <lixuanqi1995@gmail.com>
2018-10-31 15:32:26 -0700

    144

---
+++ 144-binary-tree-preorder-traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.offerFirst(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pollFirst();
+            ans.add(curr.val);
+            if (curr.right != null) {
+                stack.offerFirst(curr.right);
+            }
+            if (curr.left != null) {
+                stack.offerFirst(curr.left);
+            }
+        }
+        return ans;
+    }
+}
