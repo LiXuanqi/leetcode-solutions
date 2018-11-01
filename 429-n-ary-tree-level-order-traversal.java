2edab2bd68484ac40f0397426802a3776efce77a
lixuanqi <lixuanqi1995@gmail.com>
2018-10-31 18:40:25 -0700

    429

---
+++ 429-n-ary-tree-level-order-traversal.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node curr = queue.poll();
+                list.add(curr.val);
+                for (Node next : curr.children) {
+                    queue.offer(next);
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
