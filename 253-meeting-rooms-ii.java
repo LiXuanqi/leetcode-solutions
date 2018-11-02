2057048b868eb20f2314ea362409951459809dd4
lixuanqi <lixuanqi1995@gmail.com>
2018-11-02 16:38:27 -0700

    253

--- 253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii.java
@@ -7,30 +7,40 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
 class Solution {
     public int minMeetingRooms(Interval[] intervals) {
-        if (intervals == null) {
-            return -1;
+        if (intervals == null || intervals.length == 0) {
+            return 0;
         }
-        if (intervals.length <= 1) {
-            return intervals.length;
-        }
-        // sort by start;
-        Arrays.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval interval1, Interval interval2) {
-                return interval1.start < interval2.start ? -1 : 1;
-            }
+        Arrays.sort(intervals, (interval1, interval2) -> {
+            return interval1.start < interval2.start ? -1 : 1;
         });
-        // min heap for end time.
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        heap.offer(intervals[0].end);
-        for (int i = 1; i < intervals.length; i++) {
-            int lastTime = heap.peek();
-            if (lastTime <= intervals[i].start) {
-                heap.poll();
+        PriorityQueue<Interval> heap = new PriorityQueue<>((interval1, interval2) -> {
+            return interval1.end < interval2.end ? -1 : 1;
+        });
+        for (Interval interval : intervals) {
+            if (heap.isEmpty()) {
+                heap.offer(interval);
+            } else {
+                Interval top = heap.peek();
+                // need new room
+                if (top.end > interval.start) {
+                    heap.offer(interval);
+                } else {
+                    Interval prev = heap.poll();
+                    prev.end = interval.end;
+                    heap.offer(prev);
+                }
             }
-            heap.offer(intervals[i].end);
         }
         return heap.size();
     }
