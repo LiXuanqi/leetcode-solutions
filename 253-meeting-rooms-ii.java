8dd553736b8519b69601d7c68c2b58751d16d222
lixuanqi <lixuanqi1995@gmail.com>
2018-11-02 16:38:50 -0700

    253 lambda

--- 253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii.java
@@ -7,15 +7,6 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         if (intervals == null || intervals.length == 0) {
