b6a6446b9d7df57c197e3edd86828f75562bc76b
lixuanqi <lixuanqi1995@gmail.com>
2018-11-02 17:34:43 -0700

    680

--- 680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii.java
@@ -1,24 +1,24 @@
 class Solution {
-  public boolean validPalindrome(String s) {
-      int start = 0;
-      int end = s.length() - 1;
-      while (start <= end) {
-          if (s.charAt(start) != s.charAt(end)) {
-              return isPalindrome(s, start, end - 1) || isPalindrome(s, start + 1, end);
-          }
-          start++;
-          end--;
-      }
-      return true;
-  }
-  private boolean isPalindrome(String str, int start, int end) {
-      while (start <= end) {
-          if (str.charAt(start) != str.charAt(end)) {
-              return false;
-          }
-          start++;
-          end--;
-      }
-      return true;
-  }
+    public boolean validPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return isValidPalindrome(s, start + 1, end) || isValidPalindrome(s, start, end - 1);
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    private boolean isValidPalindrome(String str, int start, int end) {
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
