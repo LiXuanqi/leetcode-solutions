5657702b5a07dd1557bd8777d4454c729c61a932
lixuanqi <lixuanqi1995@gmail.com>
2018-11-25 21:35:08 -0800

    493

--- 493-reverse-pairs.java
+++ 493-reverse-pairs.java
@@ -1,12 +1,11 @@
 class Solution {
-    int count = 0;
+    int ans = 0;
     public int reversePairs(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length <= 1) {
             return 0;
         }
-        
         mergeSort(nums, 0, nums.length - 1, new int[nums.length]);
-        return count;
+        return ans;
     }
     private void mergeSort(int[] nums, int start, int end, int[] temp) {
         if (start >= end) {
@@ -16,40 +15,43 @@ class Solution {
         mergeSort(nums, start, mid, temp);
         mergeSort(nums, mid + 1, end, temp);
         
-        helper(nums, start, mid, end);
+        helper(nums, start, end);
         
-        merge(nums, start, mid, end, temp);
+        merge(nums, start, end, temp);
     }
-    private void helper(int[] nums, int start, int mid, int end) {
-        int index = mid + 1;
-        int c = 0;
+    // i < j , nums[i] > 2 * nums[j]
+    private void helper(int[] nums, int start, int end) {
+        int mid = start + (end - start) / 2;
+        int right = mid + 1;
+        int count = 0;
         for (int i = start; i <= mid; i++) {
-            while (index <= end && (long) nums[i] > (long) 2 * nums[index]) {
-                index++;
-                c++;
+            while (right <= end && (long) nums[i] > 2 * (long) nums[right]) {
+                count++;
+                right++;
             }
-            count = count + c;
+            ans += count;
         }
     }
-    private void merge(int[] nums, int start, int mid, int end, int[] temp) {
+    private void merge(int[] nums, int start, int end, int[] temp) {
+        int mid = start + (end - start) / 2;
         int index = start;
-        int i = start;
-        int j = mid + 1;
-        while (i <= mid && j <= end) {
-            if (nums[i] > nums[j]) {
-                temp[index++] = nums[j++];
+        int pointer1 = start;
+        int pointer2 = mid + 1;
+        while (pointer1 <= mid && pointer2 <= end) {
+            if (nums[pointer1] < nums[pointer2]) {
+                temp[index++] = nums[pointer1++];
             } else {
-                temp[index++] = nums[i++];
-            }    
+                temp[index++] = nums[pointer2++];
+            }
         }
-        while (i <= mid) {
-            temp[index++] = nums[i++];
+        while (pointer1 <= mid) {
+            temp[index++] = nums[pointer1++];
         }
-        while (j <= end) {
-            temp[index++] = nums[j++];
+        while (pointer2 <= end) {
+            temp[index++] = nums[pointer2++];
         }
-        for (int k = start; k <= end; k++) {
-            nums[k] = temp[k];
+        for (int i = start; i <= end; i++) {
+            nums[i] = temp[i];
         }
     }
 }
