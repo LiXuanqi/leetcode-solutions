077a8270bc2d44447c3d20bdb59c7413c5d9731c
lixuanqi <lixuanqi1995@gmail.com>
2018-11-29 17:02:06 -0800

    760

---
+++ 760-find-anagram-mappings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] anagramMappings(int[] A, int[] B) {
+        if (A == null || B == null) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < B.length; i++) {
+            map.put(B[i], i);
+        }
+        int[] ans = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            ans[i] = map.get(A[i]);
+        }
+        return ans;
+    }
+}
