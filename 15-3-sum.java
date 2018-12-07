363e23fbc3d9d8f3a3d874a280f65babd0dd9425
lixuanqi <lixuanqi1995@gmail.com>
2018-12-06 23:30:04 -0800

    15 170

--- 15-3-sum.java
+++ 15-3-sum.java
@@ -1,41 +1,48 @@
 class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-      List<List<Integer>> result = new ArrayList<>();
-      if (nums == null) {
-          return result;
-      }
-      Arrays.sort(nums);
-      for (int i = 0; i < nums.length - 2; i++) {
-          if (i > 0 && nums[i] == nums[i - 1]) {
-              continue;
-          }
-          int target = -nums[i];
-          int left = i + 1;
-          int right = nums.length - 1;
-          while (left < right) {
-              if (nums[left] + nums[right] == target) {
-                  List<Integer> temp = new ArrayList<>();
-                  temp.add(nums[i]);
-                  temp.add(nums[left]);
-                  temp.add(nums[right]);
-                  result.add(temp);
-                  left++;
-                  right--;
-                  while (left < right && nums[left] == nums[left - 1]) {
-                      left++;
-                  }
-                  while (left < right && nums[right] == nums[right + 1]) {
-                      right--;
-                  }
-              } else if (nums[left] + nums[right] < target) {
-                  left++;
-              } else {
-                  right--;
-              }
-          }
-      }
-      return result;
-   
-  }
-
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length < 2) {
+            return ans;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            // avoid repeated answer.
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            List<List<Integer>> lists = twoSum(nums, i + 1, -nums[i]);
+            for (List<Integer> list : lists) {
+                list.add(0, nums[i]);
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+    private List<List<Integer>> twoSum(int[] nums, int start, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int left = start;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (left != start && nums[left] == nums[left - 1]) {
+                left++;
+                continue;
+            }
+            if (right != nums.length - 1 && nums[right] == nums[right + 1]) {
+                right--;
+                continue;
+            }
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                List<Integer> list = new LinkedList<>();
+                list.add(nums[left++]);
+                list.add(nums[right--]);
+                ans.add(list);
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
 }
--- 170-two-sum-ii-data-structure-design.java
+++ 170-two-sum-ii-data-structure-design.java
@@ -1,23 +1,20 @@
 class TwoSum {
     private Map<Integer, Integer> map;
-    private List<Integer> list;
     /** Initialize your data structure here. */
     public TwoSum() {
         this.map = new HashMap<>();
-        this.list = new ArrayList<>();
     }
     
     /** Add the number to an internal data structure.. */
     public void add(int number) {
-        list.add(number);
-        map.put(number, list.size() - 1);
+        map.put(number, map.getOrDefault(number, 0) + 1);
     }
     
     /** Find if there exists any pair of numbers which sum is equal to the value. */
     public boolean find(int value) {
-        for (int i = 0; i < list.size(); i++) {
-            int wanted = value - list.get(i);
-            if (map.containsKey(wanted) && map.get(wanted) != i) {
+        for (int key : map.keySet()) {
+            int need = value - key;
+            if ((need == key && map.get(need) > 1) || (need != key && map.containsKey(need))) {
                 return true;
             }
         }
