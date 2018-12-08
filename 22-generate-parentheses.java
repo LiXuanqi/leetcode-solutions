c78fca26edbe9f7cc1f63f09a4283f4715a65624
lixuanqi <lixuanqi1995@gmail.com>
2018-12-07 21:55:41 -0800

    22

--- 22-generate-parentheses.java
+++ 22-generate-parentheses.java
@@ -1,39 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> solutions = new ArrayList<>();
-        if (n < 0) {
-            return solutions;
+        List<String> list = new ArrayList<>();
+        if (n <= 0) {
+            return list;
         }
-        if (n == 0) {
-            solutions.add("");
-            return solutions;
-        }
-        dfs(new StringBuilder(), 0, 0, n, solutions);
-        return solutions;
+        dfs(n, new StringBuilder(), 0, 0, list);
+        return list;
     }
-    private void dfs(
-        StringBuilder sb,
-        int left,
-        int right,
-        int n,
-        List<String> solutions
-    ) {
-        if (left + right > 2 * n) {
-            return;
-        }
-        
-        if (left == right && left + right == 2 * n) {
-            solutions.add(sb.toString());
+    private void dfs(int n, StringBuilder sb, int left, int right, List<String> result) {
+        if (left + right == n * 2) {
+            if (left == right) {
+                result.add(sb.toString());
+            }
             return;
         }
         if (left < right) {
             return;
         }
         sb.append('(');
-        dfs(sb, left + 1, right, n, solutions);
+        dfs(n, sb, left + 1, right, result);
         sb.deleteCharAt(sb.length() - 1);
         sb.append(')');
-        dfs(sb, left, right + 1, n, solutions);
+        dfs(n, sb, left, right + 1, result);
         sb.deleteCharAt(sb.length() - 1);
     }
 }
