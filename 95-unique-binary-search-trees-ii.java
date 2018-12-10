3a4bae0760a67b77a8eeb034aaaad2158743d8a1
lixuanqi <lixuanqi1995@gmail.com>
2018-12-10 14:15:04 -0800

    95

---
+++ 95-unique-binary-search-trees-ii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<TreeNode>();
+        }
+        return generate(1, n); 
+    }
+    private List<TreeNode> generate(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> lefts = generate(start, i - 1);
+            List<TreeNode> rights = generate(i + 1, end);
+            for (TreeNode left : lefts) {
+                for (TreeNode right : rights) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
