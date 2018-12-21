819e009370a3a39d9650d633f81b319da36e64e7
lixuanqi <lixuanqi1995@gmail.com>
2018-12-21 15:32:12 -0800

    14

--- 14-longest-common-prefix.java
+++ 14-longest-common-prefix.java
@@ -3,21 +3,20 @@ class Solution {
         if (strs == null || strs.length == 0) {
             return "";
         }
-        String commonPrefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            commonPrefix = findCommonPrefix(commonPrefix, strs[i]);
+        String ans = null;
+        for (String str : strs) {
+            ans = findCommon(ans, str);
         }
-        return commonPrefix;
+        return ans;
     }
-    private String findCommonPrefix(String str1, String str2) {
-        int count = 0;
-        int i = 0;
-        int j = 0;
-        while (i < str1.length() && j < str2.length() && str1.charAt(i) == str2.charAt(j)) {
-            count++;
-            i++;
-            j++;
+    private String findCommon(String str1, String str2) {
+        if (str1 == null) {
+            return str2;
         }
-        return str1.substring(0, count);
+        int i = 0;
+        while (i < str1.length() && i < str2.length() && str1.charAt(i) == str2.charAt(i)) {
+            i++;
+        }
+        return str1.substring(0, i);
     }
 }
