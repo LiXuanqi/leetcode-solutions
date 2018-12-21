7be629607c0a2d18dbc9bec57d1684066bff1fb4
lixuanqi <lixuanqi1995@gmail.com>
2018-12-20 22:26:16 -0800

    179

---
+++ 179-largest-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+        // int[] => String[]
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < strs.length; i++) {
+            strs[i] = "" + nums[i];
+        }
+        Arrays.sort(strs, (str1, str2) -> {
+            return (str2 + str1).compareTo(str1 + str2);
+        });
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str);
+        }
+        
+        String ans = sb.toString();
+        int i = 0;
+        while (i < ans.length() - 1 && ans.charAt(i) == '0') {
+            i++;
+        }
+        return ans.substring(i);
+    }
+}
