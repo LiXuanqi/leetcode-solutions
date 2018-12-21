fe98c912f89dd7b7669368e99d5d04037757d730
lixuanqi <lixuanqi1995@gmail.com>
2018-12-21 13:01:51 -0800

    347

--- 347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements.java
@@ -1,37 +1,29 @@
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         if (nums == null || nums.length == 0 || k <= 0) {
-            return result;
+            return ans;
         }
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        // System.out.println(map);
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int frequency = entry.getValue();
-            if (buckets[frequency] == null) {
-                buckets[frequency] = new ArrayList<>();
+        List[] bukkits = new List[nums.length + 1];
+        for (Integer key : map.keySet()) {
+            if (bukkits[map.get(key)] == null) {
+                bukkits[map.get(key)] = new ArrayList<>();
             }
-            buckets[frequency].add(entry.getKey());
+            bukkits[map.get(key)].add(key);
         }
-        
-        for (int i = buckets.length - 1; i >= 0; i--) {
-            if (result.size() >= k) {
+        for (int i = bukkits.length - 1; i >= 0; i--) {
+            if (k <= 0) {
                 break;
             }
-            if (buckets[i] != null) {
-                for (int num : buckets[i]) {
-                    // System.out.println(num);
-                    if (result.size() >= k) {
-                        break;
-                    }
-                    result.add(num);
-                }
+            if (bukkits[i] != null) {
+                ans.addAll(bukkits[i]);
+                k -= bukkits[i].size();
             }
         }
-        return result;
+        return ans;
     }
 }
