ca916f6af985825cea8e42645705e1ff07aae1af
lixuanqi <lixuanqi1995@gmail.com>
2018-12-21 08:50:21 -0800

    139

--- 139-word-break.java
+++ 139-word-break.java
@@ -1,28 +1,23 @@
 class Solution {
-  public boolean wordBreak(String s, List<String> wordDict) {
-      if (s == null || s.length() == 0) {
-          return true;
-      }
-      if (wordDict == null || wordDict.size() == 0) {
-          return false;
-      }
-      Set<String> set = new HashSet<>(wordDict);
-      int maxLength = 0;
-      for (String word : wordDict) {
-          if (word.length() > maxLength) {
-              maxLength = word.length();
-          }
-      }
-      boolean[] dp = new boolean[s.length() + 1];
-      dp[0] = true;
-      for (int i = 1; i <= s.length(); i++) {
-          dp[i] = false;
-          for (int j = Math.max(0, i - maxLength - 1); j < i; j++) {
-              if (dp[j] && set.contains(s.substring(j, i))) {
-                  dp[i] = true;
-              }
-          }
-      }
-      return dp[dp.length - 1];
-  }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        if (wordDict == null || wordDict.size() == 0) {
+            return false;
+        }
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < dp.length; i++) {
+            boolean isMatch = false;
+            for (int j = 0; j < wordDict.size(); j++) {
+                String word = wordDict.get(j);
+                if (i - word.length() >= 0 && dp[i - word.length()] && word.equals(s.substring(i - word.length(), i))) {
+                    isMatch = true;
+                }
+            }
+            dp[i] = isMatch;
+        }
+        return dp[dp.length - 1];                                                                               
+    }
 }
