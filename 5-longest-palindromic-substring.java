b267c5b15a606564e202f941721e1fe5ffeef3db
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 13:11:24 -0800

    5

--- 5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring.java
@@ -6,15 +6,15 @@ class Solution {
         String ans = "";
         int maxLength = 0;
         for (int i = 0; i < s.length(); i++) {
+            // 1  2  1
             int length = helper(s, i, i);
-            // update ans.
             if (length > maxLength) {
                 maxLength = length;
                 ans = s.substring(i - length / 2, i + length / 2 + 1);
             }
-            if (i != 0) {
+            if (i > 0) {
                 length = helper(s, i - 1, i);
-                // update ans.
+                // 1 2 2 1
                 if (length > maxLength) {
                     maxLength = length;
                     ans = s.substring(i - length / 2, i + length / 2);
@@ -23,12 +23,12 @@ class Solution {
         }
         return ans;
     }
-    private int helper(String s, int left, int right) {
+    private int helper(String str, int left, int right) {
         int length = 0;
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            length += (left == right ? 1 : 2); // Notice left == right and update length before left-- right++.
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            length += left == right ? 1 : 2;
             left--;
-            right++; 
+            right++;
         }
         return length;
     }
