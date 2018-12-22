ff7ecb4d9e4486e13eed367a6b6fd403b4acb80b
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 11:51:55 -0800

    46

--- 46-permutaions.java
+++ 46-permutaions.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         if (nums == null || nums.length == 0) {
-            return result;
+            return ans;
         }
-        dfs(nums, new boolean[nums.length], new ArrayList<Integer>(), result);
-        return result;
+        dfs(nums, new boolean[nums.length], new ArrayList<>(), ans);
+        return ans;
     }
-    private void dfs(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> solutions) {
+    private void dfs(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> ans) {
         if (temp.size() == nums.length) {
-            solutions.add(new ArrayList<Integer>(temp));
+            ans.add(new ArrayList<>(temp));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (used[i]) {
-                continue;
+            if (!used[i]) {
+                temp.add(nums[i]);
+                used[i] = true;
+                dfs(nums, used, temp, ans);
+                temp.remove(temp.size() - 1);
+                used[i] = false;
             }
-            temp.add(nums[i]);
-            used[i] = true;
-            dfs(nums, used, temp, solutions);
-            temp.remove(temp.size() - 1);
-            used[i] = false;
         }
     }
 }
-
-// Time = O(n!)
-// Space = O(n)(call stack) + O(n)(boolean[] used)
