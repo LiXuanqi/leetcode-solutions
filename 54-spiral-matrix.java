2e1b4b9166b74ddbcbe6b82edda5ea062d30c09f
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 14:06:39 -0800

    54

--- 54-spiral-matrix.java
+++ 54-spiral-matrix.java
@@ -1,43 +1,46 @@
 class Solution {
-  public List<Integer> spiralOrder(int[][] matrix) {
-      if (matrix == null) {
-          return null;
-      }
-      if (matrix.length == 0 || matrix[0].length == 0) {
-          return new ArrayList<Integer>();
-      }
-      return printSpiralCircle(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1);
-  }
-  private List<Integer> printSpiralCircle(int[][] matrix, int startRow, int startCol, int endRow, int endCol) {
-      List<Integer> level = new ArrayList<>();
-      if (startRow > endRow || startCol > endCol) {
-          return level;
-      }
-      if (startRow == endRow) {
-          for (int i = startCol; i <= endCol; i++) {
-              level.add(matrix[startRow][i]);
-          }
-          return level;
-      }
-      if (startCol == endCol) {
-          for (int i = startRow; i <= endRow; i++) {
-              level.add(matrix[i][startCol]);
-          }
-          return level;
-      }
-      for (int i = startCol; i < endCol; i++) {
-          level.add(matrix[startRow][i]);
-      }
-      for (int i = startRow; i < endRow; i++) {
-          level.add(matrix[i][endCol]);
-      }
-      for (int i = endCol; i > startCol; i--) {
-          level.add(matrix[endRow][i]);
-      }
-      for (int i = endRow; i > startRow; i--) {
-          level.add(matrix[i][startCol]);
-      }
-      level.addAll(printSpiralCircle(matrix, startRow + 1, startCol + 1,  endRow - 1, endCol - 1));
-      return level;
-  }
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return Collections.EMPTY_LIST;
+        }
+        return printCircle(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1);
+    }
+    private List<Integer> printCircle(int[][] matrix, int startRow, int startCol, int endRow, int endCol) {
+        if (startRow > endRow || startCol > endCol) {
+            return Collections.EMPTY_LIST;
+        }
+        List<Integer> ans = new ArrayList<>();
+        if (startRow == endRow) {
+            for (int i = startCol; i <= endCol; i++) {
+                ans.add(matrix[startRow][i]);
+            }
+            return ans;
+        }
+        if (startCol == endCol) {
+            for (int i = startRow; i <= endRow; i++) {
+                ans.add(matrix[i][startCol]);
+            }
+            return ans;
+        }
+        // print circle.
+        // top
+        for (int i = startCol; i < endCol; i++) {
+            ans.add(matrix[startRow][i]);
+        }
+        // right
+        for (int i = startRow; i< endRow; i++) {
+            ans.add(matrix[i][endCol]);
+        }
+        // down
+        for (int i = endCol; i > startCol; i--) {
+            ans.add(matrix[endRow][i]);
+        }
+        // left
+        for (int i = endRow; i > startRow; i--) {
+            ans.add(matrix[i][startCol]);
+        }
+        List<Integer> rest = printCircle(matrix, startRow + 1, startCol + 1, endRow - 1, endCol - 1);
+        ans.addAll(rest);
+        return ans;
+    }
 }
