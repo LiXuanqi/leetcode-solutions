f043a74459fc399d2085142ba368cf506ce6febd
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 13:41:42 -0800

    13

--- 13-roman-to-Integer.java
+++ 13-roman-to-Integer.java
@@ -1,21 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('M', 1000);
-        map.put('D', 500);
-        map.put('C', 100);
-        map.put('L', 50);
-        map.put('X', 10);
-        map.put('V', 5);
-        map.put('I', 1);
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i < (s.length() - 1) && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
-                result = result - map.get(s.charAt(i));
-            } else {
-                result = result + map.get(s.charAt(i));
-            }
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-        return result;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int ans = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            ans += map.get(s.charAt(i)) < map.get(s.charAt(i + 1)) ? -map.get(s.charAt(i)) : map.get(s.charAt(i));
+        }
+        ans += map.get(s.charAt(s.length() - 1));
+        return ans;
     }
 }
