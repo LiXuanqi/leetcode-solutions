f688ccc126eec15937115657b0e7873b9cc7d4a4
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 11:37:34 -0800

    21 53

--- 21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists.java
@@ -35,31 +35,34 @@ class Solution {
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                tail.next = l1;
-                tail = tail.next;
-                l1 = l1.next; 
+                curr.next = l1;
+                l1 = l1.next;
             } else {
-                tail.next = l2;
-                tail = tail.next;
+                curr.next = l2;
                 l2 = l2.next;
             }
+            curr = curr.next;
         }
         while (l1 != null) {
-            tail.next = l1;
-            tail = tail.next;
-            l1 = l1.next; 
+            curr.next = l1;
+            l1 = l1.next;
+            curr = curr.next;
         }
         while (l2 != null) {
-            tail.next = l2;
-            tail = tail.next;
+            curr.next = l2;
             l2 = l2.next;
+            curr = curr.next;
         }
-        tail.next = null;
         return dummy.next;
     }
 }
--- 53-maximum-subarray.java
+++ 53-maximum-subarray.java
@@ -1,17 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            if (sum < 0) {
-                sum = nums[i];
-            } else {
-                sum += nums[i];
-            }
-            max = Math.max(max, sum);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = dp[i - 1] < 0 ? nums[i] : dp[i - 1] + nums[i];
+            max = Math.max(max, dp[i]);
         }
         return max;
     }
