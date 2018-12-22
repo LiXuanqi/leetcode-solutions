37efac62af581a787a4c70bde43b327b584781f2
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 12:37:05 -0800

    1

--- 1-two-sum.java
+++ 1-two-sum.java
@@ -3,14 +3,16 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return null;
         }
+        int[] ans = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int need = target - nums[i];
             if (map.containsKey(need)) {
-                return new int[] {map.get(need), i};
-            } else {
-                map.put(nums[i], i);
+                ans[0] = map.get(need);
+                ans[1] = i;
+                return ans;
             }
+            map.put(nums[i], i);
         }
         return null;
     }
