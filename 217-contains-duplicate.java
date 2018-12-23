466b2a39dd60790ca879d46a7e80afa35d7390e5
lixuanqi <lixuanqi1995@gmail.com>
2018-12-23 01:37:34 -0800

    217

--- 217-contains-duplicate.java
+++ 217-contains-duplicate.java
@@ -2,16 +2,15 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         if (nums == null || nums.length == 0) {
             return false;
-        }
+        } 
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
-            if (!set.add(num)) {
+            if (set.contains(num)) {
                 return true;
+            } else {
+                set.add(num);
             }
         }
         return false;
     }
 }
-
-// Time = O(n)
-// Space = O(n)
