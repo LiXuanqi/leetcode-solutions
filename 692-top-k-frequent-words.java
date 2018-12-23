7812f26016b5171ab3c42f97734bb35542d2c885
lixuanqi <lixuanqi1995@gmail.com>
2018-12-23 01:26:20 -0800

    692

--- 692-top-k-frequent-words.java
+++ 692-top-k-frequent-words.java
@@ -1,40 +1,37 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        List<String> result = new ArrayList<>();
-        if (words == null || words.length == 0) {
-            return result;
+        List<String> ans = new ArrayList<>();
+        if (words == null || words.length == 0 || k <= 0) {
+            return ans;
         }
         Map<String, Integer> map = new HashMap<>();
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        
-        List<String>[] bukkit = new List[words.length];
-        
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            int frequency = entry.getValue();
-            if (bukkit[frequency] == null) {
-                bukkit[frequency] = new ArrayList<>();
+        List<String>[] bukkits = new List[words.length + 1];
+        for (String key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bukkits[frequency] == null) {
+                bukkits[frequency] = new ArrayList<>();
             }
-            bukkit[frequency].add(entry.getKey());
+            bukkits[frequency].add(key);
         }
-        int count = 0;
-        for (int i = bukkit.length - 1; i >= 0; i--) {
-            if (bukkit[i] == null) {
-                continue;
+        for (int i = bukkits.length - 1; i>= 0; i--) {
+            if (k <= 0) {
+                break;
             }
-            Collections.sort(bukkit[i]);
-            for (String str : bukkit[i]) {
-                result.add(str);
-                count++;
-                if (count == k) {
-                    
-                    return result;
+            if (bukkits[i] != null) {
+
+                Collections.sort(bukkits[i]);
+                for (int j = 0; j < bukkits[i].size(); j++) {
+                    if (k <= 0) {
+                        break;
+                    }
+                    ans.add(bukkits[i].get(j));
+                    k--;
                 }
             }
         }
-     
-        
-        return result;
+        return ans;
     }
 }
