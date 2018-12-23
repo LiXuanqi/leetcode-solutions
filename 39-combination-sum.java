845807b984dbcc67278ca76a0a9e792382b1ba29
lixuanqi <lixuanqi1995@gmail.com>
2018-12-23 01:16:04 -0800

    39

--- 39-combination-sum.java
+++ 39-combination-sum.java
@@ -4,20 +4,21 @@ class Solution {
         if (candidates == null || candidates.length == 0) {
             return ans;
         }
+        Arrays.sort(candidates);
         dfs(candidates, 0, target, new ArrayList<>(), ans);
         return ans;
     }
-    private void dfs(int[] nums, int start, int target, List<Integer> temp, List<List<Integer>> list) {
+    private void dfs(int[] nums, int start, int target, List<Integer> temp, List<List<Integer>> ans) {
         if (target < 0) {
             return;
         }
         if (target == 0) {
-            list.add(new ArrayList<>(temp));
+            ans.add(new ArrayList<>(temp));
             return;
         }
         for (int i = start; i < nums.length; i++) {
             temp.add(nums[i]);
-            dfs(nums, i, target - nums[i], temp, list);
+            dfs(nums, i, target - nums[i], temp, ans);
             temp.remove(temp.size() - 1);
         }
     }
