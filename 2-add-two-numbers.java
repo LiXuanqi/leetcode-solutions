18b8aba112bb93eda2a7cd18f548dadff923163e
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 19:54:58 -0800

    2

--- 2-add-two-numbers.java
+++ 2-add-two-numbers.java
@@ -8,47 +8,41 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        }
         if (l1 == null) {
             return l2;
         }
         if (l2 == null) {
             return l1;
         }
+        ListNode pointer1 = l1;
+        ListNode pointer2 = l2;
         ListNode dummy = new ListNode(-1);
-        ListNode c1 = l1;
-        ListNode c2 = l2;
         ListNode curr = dummy;
-        int count = 0;
-        while (c1 != null && c2 != null) {
-            int sum = c1.val + c2.val + count;
-            count = sum / 10;
-            sum %= 10;
-            curr.next = new ListNode(sum);
+        int rest = 0;
+        while (pointer1 != null && pointer2 != null) {
+            int sum = rest + pointer1.val + pointer2.val;
+            rest = sum / 10;
+            curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            c1 = c1.next;
-            c2 = c2.next;
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
         }
-        while (c1 != null) {
-            int sum = c1.val + count;
-            count = sum / 10;
-            sum %= 10;
-            curr.next = new ListNode(sum);
+        while (pointer1 != null) {
+            int sum = rest + pointer1.val;
+            rest = sum / 10;
+            curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            c1 = c1.next;
+            pointer1 = pointer1.next;
         }
-        while (c2 != null) {
-            int sum = c2.val + count;
-            count = sum / 10;
-            sum %= 10;
-            curr.next = new ListNode(sum);
+        while (pointer2 != null) {
+            int sum = rest + pointer2.val;
+            rest = sum / 10;
+            curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            c2 = c2.next;
+            pointer2 = pointer2.next; 
         }
-        if (count != 0) {
-            curr.next = new ListNode(count);
+        if (rest != 0) {
+            curr.next = new ListNode(rest);
         }
         return dummy.next;
     }
