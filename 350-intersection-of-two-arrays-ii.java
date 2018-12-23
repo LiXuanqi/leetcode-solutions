4ca24cdf5a30ca90df1fd5fcbf26a3efdda2a1f2
lixuanqi <lixuanqi1995@gmail.com>
2018-12-22 16:13:12 -0800

    350

--- 350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii.java
@@ -1,32 +1,19 @@
 class Solution {
-  public int[] intersect(int[] nums1, int[] nums2) {
-      int[] result = new int[0];
-      if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
-          return result;
-      }
-      ArrayList<Integer> list = new ArrayList<>();
-      
-      HashMap<Integer, Integer> map = new HashMap<>();
-      
-      for (int num : nums1) {2
-          if (!map.containsKey(num)) {
-              map.put(num, 0);
-          }
-          map.put(num, map.get(num) + 1);
-      }
-      
-      for (int num : nums2) {
-          if (map.containsKey(num) && map.get(num) != 0) {
-              list.add(num);
-              map.put(num, map.get(num) - 1);
-          } 
-      }
-      
-      result = list.stream().mapToInt(i -> i).toArray();
-      return result;
-  }
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                ans.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
 }
-
-// O(n+m);
-// Space ?
-// how about follow up?
