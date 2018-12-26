18aaf418b3f2364486fe0531037009304681a6b2
lixuanqi <lixuanqi1995@gmail.com>
2018-12-25 18:22:15 -0800

    362

--- 362-design-hit-counter.java
+++ 362-design-hit-counter.java
@@ -29,3 +29,48 @@ class HitCounter {
  * obj.hit(timestamp);
  * int param_2 = obj.getHits(timestamp);
  */
+
+ class HitCounter {
+    
+    private static final int INTERVAL = 300;
+    
+    private int[] times;
+    private int[] hits;
+    
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        times = new int[INTERVAL];
+        hits = new int[INTERVAL];
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        int index = timestamp % INTERVAL;
+        if (times[index] == timestamp) {
+            hits[index]++;
+        } else {
+            hits[index] = 1;
+            times[index] = timestamp;
+        }
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        int total = 0;
+        for (int i = 0; i < hits.length; i++) {
+            if (timestamp - times[i] < INTERVAL) {
+                total += hits[i];
+            }
+        }
+        return total;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
