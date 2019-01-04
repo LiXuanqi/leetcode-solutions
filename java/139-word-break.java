c222c74444d7be1d1dd10ef1aa50d1bac835c861
lixuanqi <lixuanqi1995@gmail.com>
2019-01-03 17:48:17 -0800

    139

--- java/139-word-break.java
+++ java/139-word-break.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        if (s == null || s.length() == 0) {
-            return true;
-        }
-        if (wordDict == null || wordDict.size() == 0) {
+        if (s == null || s.length() == 0 || wordDict == null || wordDict.size() == 0) {
             return false;
         }
+        int maxLength = getMaxLength(wordDict);
+        Set<String> dict = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for (int i = 1; i < dp.length; i++) {
             boolean isMatch = false;
-            for (int j = 0; j < wordDict.size(); j++) {
-                String word = wordDict.get(j);
-                if (i - word.length() >= 0 && dp[i - word.length()] && word.equals(s.substring(i - word.length(), i))) {
+            for (int j = Math.max(0, i - maxLength); j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
                     isMatch = true;
                 }
             }
             dp[i] = isMatch;
         }
-        return dp[dp.length - 1];                                                                               
+        return dp[dp.length - 1];
+    }
+    private int getMaxLength(List<String> words) {
+        int max = Integer.MIN_VALUE;
+        for (String word : words) {
+            max = Math.max(word.length(), max);
+        }
+        return max;
     }
 }
