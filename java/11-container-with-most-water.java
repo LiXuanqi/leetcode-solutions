ca17b7ad48378205c5bfab88d60396fbcfd331d5
lixuanqi <lixuanqi1995@gmail.com>
2019-01-08 09:19:10 -0800

    11

--- java/11-container-with-most-water.java
+++ java/11-container-with-most-water.java
@@ -1,22 +1,21 @@
 class Solution {
-  public int maxArea(int[] height) {
-      if (height == null || height.length <= 1) {
-          return 0;
-      }
-      int left = 0;
-      int right = height.length - 1;
-      int maxArea = Integer.MIN_VALUE;
-      while (left < right) {
-          int area = Math.min(height[left], height[right]) * (right - left);
-          if (area > maxArea) {
-              maxArea = area;
-          }
-          if (height[left] <= height[right]) {
-              left++;
-          } else {
-              right--;
-          }
-      }
-      return maxArea;
-  }
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        int ans = Integer.MIN_VALUE;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int area = h * (right - left);
+            ans = Math.max(ans, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
 }
