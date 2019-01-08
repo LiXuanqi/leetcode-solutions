cc2a363c8006342b63be628d62b7b43dc08f465a
lixuanqi <lixuanqi1995@gmail.com>
2019-01-08 10:10:22 -0800

    219

--- java/219-contains-duplicate-ii.java
+++ java/219-contains-duplicate-ii.java
@@ -1,21 +1,18 @@
 class Solution {
-  public boolean containsNearbyDuplicate(int[] nums, int k) {
-      if (nums == null || nums.length == 0) {
-          return false;
-      }
-      Map<Integer, Integer> map = new HashMap<>();
-      for (int i = 0; i < nums.length; i++) {
-          if (map.containsKey(nums[i])) {
-              int index = map.get(nums[i]);              
-              if (i - index <= k) {
-                  return true;
-              } 
-          }
-          map.put(nums[i], i);
-      }
-      return false;
-  }
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int index = map.get(nums[i]);
+                if (i - index <= k) {
+                    return true;
+                }
+            } 
+            map.put(nums[i], i);
+        }
+        return false;
+    }
 }
-
-// Space = O(2n)
-// Time = O(n);
