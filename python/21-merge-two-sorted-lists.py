bccf88dbd6c3b251de99ec6c2674b2ae2e404360
lixuanqi <lixuanqi1995@gmail.com>
2019-01-13 21:07:32 -0800

    21

---
+++ python/21-merge-two-sorted-lists.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        curr = dummy
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        while l1 != None:
+            curr.next = l1
+            l1 = l1.next
+            curr = curr.next
+        while l2 != None:
+            curr.next = l2
+            l2 = l2.next
+            curr = curr.next
+        return dummy.next
