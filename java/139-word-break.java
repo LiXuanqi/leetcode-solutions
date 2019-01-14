bfa007f40a4c59d7e4367537d733033c777696e4
lixuanqi <lixuanqi1995@gmail.com>
2019-01-14 14:42:29 -0800

    139

--- java/139-word-break.java
+++ java/139-word-break.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        if (s == null || s.length() == 0 || wordDict == null || wordDict.size() == 0) {
+        if (wordDict == null || wordDict.size() == 0) {
             return false;
         }
-        int maxLength = getMaxLength(wordDict);
-        Set<String> dict = new HashSet<>(wordDict);
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        // get max length
+        int maxLength = 0;
+        for (String word : wordDict) {
+            maxLength = Math.max(maxLength, word.length());
+        }
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
+        Set<String> dict = new HashSet<>(wordDict);
         for (int i = 1; i < dp.length; i++) {
-            boolean isMatch = false;
-            for (int j = Math.max(0, i - maxLength); j < i; j++) {
-                if (dp[j] && dict.contains(s.substring(j, i))) {
-                    isMatch = true;
+            int start = Math.max(0, i - maxLength);
+            for (int j = start; j < i; j++) {
+                if (dict.contains(s.substring(j, i)) && dp[j]) {
+                    dp[i] = true;
                 }
             }
-            dp[i] = isMatch;
         }
         return dp[dp.length - 1];
-    }
-    private int getMaxLength(List<String> words) {
-        int max = Integer.MIN_VALUE;
-        for (String word : words) {
-            max = Math.max(word.length(), max);
-        }
-        return max;
     }
 }
