9b7f239427cface419747972b992afbb23398f61
lixuanqi <lixuanqi1995@gmail.com>
2019-01-14 16:53:36 -0800

    599

--- java/599-minimum-index-sum-of-two-lists.java
+++ java/599-minimum-index-sum-of-two-lists.java
@@ -1,23 +1,24 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        if (list1 == null || list2 == null) {
-            return null;
+        List<String> ans = new ArrayList<>();
+        if (list1 == null || list1.length == 0 || list2 == null || list2.length == 0) {
+            return new String[0];           
         }
         Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < list1.length; i++) {
             map.put(list1[i], i);
         }
         int min = Integer.MAX_VALUE;
-        List<String> ans = new ArrayList<>();
-        for (int j = 0; j < list2.length; j++) {
-            if (map.containsKey(list2[j])) {
-                int sum = j + map.get(list2[j]);
+        for (int i = 0; i < list2.length; i++) {
+            if (map.containsKey(list2[i])) {
+                int sum = i + map.get(list2[i]);
+                if (sum == min) {
+                    ans.add(list2[i]);
+                }
                 if (sum < min) {
                     min = sum;
                     ans.clear();
-                    ans.add(list2[j]);
-                } else if (sum == min) {
-                    ans.add(list2[j]);
+                    ans.add(list2[i]);
                 }
             }
         }
