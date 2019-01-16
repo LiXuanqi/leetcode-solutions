f540168248cba123fc8dae5fd5dfdd66224026c5
lixuanqi <lixuanqi1995@gmail.com>
2019-01-16 11:23:34 -0800

    332

--- java/332-reconstruct-itinerary.java
+++ java/332-reconstruct-itinerary.java
@@ -1,55 +1,3 @@
-// Method 1: DFS
-class Solution {
-    public List<String> findItinerary(String[][] tickets) {
-        if (tickets == null || tickets.length == 0 || tickets[0].length != 2) {
-            return null;
-        }
-        Map<String, List<String>> graph = buildGraph(tickets);
-        List<String> path = new ArrayList<>();
-        path.add("JFK");
-        if (dfs(graph, path, tickets.length + 1, "JFK")) {
-            return path;
-        }
-        return null;
-    }
-    private boolean dfs(Map<String, List<String>> graph, List<String> path, int targetNum, String start) {
-        if (path.size() == targetNum) {
-            return true;
-        }
-        if (!graph.containsKey(start)) {
-            return false;
-        }
-        List<String> neighbors = graph.get(start);
-        for (int i = 0; i < neighbors.size(); i++) {
-            String neighbor = neighbors.get(i);
-            neighbors.remove(i);
-            path.add(neighbor);
-            if (dfs(graph, path, targetNum, neighbor)) {
-                return true;
-            }
-            path.remove(path.size() - 1);
-            neighbors.add(i, neighbor);
-        }
-        return false;
-    }
-    private Map<String, List<String>> buildGraph(String[][] tickets) {
-        Map<String, List<String>> graph = new HashMap<>();
-        for (String[] ticket : tickets) {
-            String from = ticket[0];
-            String to = ticket[1];
-            if (!graph.containsKey(from)) {
-                graph.put(from, new ArrayList<>());
-            }
-            graph.get(from).add(to);
-        }
-        for (List<String> list : graph.values()) {
-            Collections.sort(list);
-        }
-        return graph;
-    }
-}
-
-// Method 2: hierholzer
 class Solution {
     class Edge {
         String to;
@@ -58,26 +6,26 @@ class Solution {
             this.to = to;
         }
     }
-    private List<String> path = new LinkedList<>();
     public List<String> findItinerary(String[][] tickets) {
+        List<String> path = new LinkedList<>();
         if (tickets == null || tickets.length == 0 || tickets[0].length != 2) {
-            return null;
+            return path;
         }
         Map<String, List<Edge>> graph = buildGraph(tickets);
-        visit("JFK", graph);
+        visit(graph, "JFK", path);
         return path;
     }
-    private void visit(String start, Map<String, List<Edge>> graph) {
+    private void visit(Map<String, List<Edge>> graph, String start, List<String> path) {
+        // No flight starts from here.
         if (!graph.containsKey(start)) {
             path.add(0, start);
             return;
         }
         for (Edge edge : graph.get(start)) {
-            if (edge.used) {
-                continue;
+            if (!edge.used) {
+                edge.used = true;
+                visit(graph, edge.to, path);
             }
-            edge.used = true;
-            visit(edge.to, graph);
         }
         path.add(0, start);
     }
@@ -91,8 +39,8 @@ class Solution {
             }
             graph.get(from).add(new Edge(to));
         }
-        for (List<Edge> list : graph.values()) {
-            Collections.sort(list, (e1, e2) -> {
+        for (List<Edge> value : graph.values()) {
+            Collections.sort(value, (e1, e2) -> {
                 return e1.to.compareTo(e2.to);
             });
         }
