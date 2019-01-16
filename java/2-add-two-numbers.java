13721bcbdb7ad1bfa910638687611c26bad30fae
lixuanqi <lixuanqi1995@gmail.com>
2019-01-15 20:33:03 -0800

    2

--- java/2-add-two-numbers.java
+++ java/2-add-two-numbers.java
@@ -8,38 +8,22 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        ListNode pointer1 = l1;
-        ListNode pointer2 = l2;
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
         int rest = 0;
-        while (pointer1 != null && pointer2 != null) {
-            int sum = rest + pointer1.val + pointer2.val;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + rest;
             rest = sum / 10;
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            pointer1 = pointer1.next;
-            pointer2 = pointer2.next;
-        }
-        while (pointer1 != null) {
-            int sum = rest + pointer1.val;
-            rest = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            pointer1 = pointer1.next;
-        }
-        while (pointer2 != null) {
-            int sum = rest + pointer2.val;
-            rest = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            pointer2 = pointer2.next; 
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
         if (rest != 0) {
             curr.next = new ListNode(rest);
