2865fc05fd306836acb2ce75f81f1fbbc083684d
lixuanqi <lixuanqi1995@gmail.com>
2019-01-16 17:21:45 -0800

    205

--- java/205-isomorphic-strings.java
+++ java/205-isomorphic-strings.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
         Map<Character, Character> map = new HashMap<>();
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-            if (map.containsKey(c1)) {
-                if (map.get(c1) != c2) {
+            if (map.containsKey(s.charAt(i))) {
+                if (map.get(s.charAt(i)) != t.charAt(i)) {
                     return false;
                 }
             } else {
-                if (set.contains(c2)) {
-                    return false; // No two characters may map to the same character
-                } else {
-                    map.put(c1, c2);
-                    set.add(c2);
+                if (set.contains(t.charAt(i))) {
+                    return false;
                 }
+                map.put(s.charAt(i), t.charAt(i));
+                set.add(t.charAt(i));
             }
         }
         return true;
     }
 }
-
-// "ab"  "aa" => false
