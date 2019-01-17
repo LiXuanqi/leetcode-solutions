a8803686d50c1c8a33d8f3c0a2e8779ba2eb16e1
lixuanqi <lixuanqi1995@gmail.com>
2019-01-17 15:20:01 -0800

    200

--- java/200-number-of-islands.java
+++ java/200-number-of-islands.java
@@ -7,46 +7,41 @@ class Solution {
             this.y = y;
         }
     }
-    private int count = 0;
+    int[] directionX = {0, 0, 1, -1};
+    int[] directionY = {1, -1, 0, 0};
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    findIsland(grid, i, j);
-                }
+                    bfs(grid, i, j);
+                    count++;
+                } 
             }
         }
         return count;
     }
-    private void findIsland(char[][] grid, int startX, int startY) {
-        int[] directionX = {0, 0, 1, -1};
-        int[] directionY = {1, -1, 0, 0};
+    private void bfs(char[][] grid, int x, int y) {
         Queue<Coordinate> queue = new LinkedList<>();
-        count++;
-        queue.offer(new Coordinate(startX, startY));
-        grid[startX][startY] = '0';
+        queue.offer(new Coordinate(x, y));
+        grid[x][y] = '0';
         while (!queue.isEmpty()) {
             Coordinate curr = queue.poll();
-           
-            // expand
             for (int i = 0; i < 4; i++) {
                 int nextX = curr.x + directionX[i];
                 int nextY = curr.y + directionY[i];
-                if (!isValid(grid, nextX, nextY)) {
-                    continue;
+                if (inBound(nextX, nextY, grid) && grid[nextX][nextY] == '1') {
+                    queue.offer(new Coordinate(nextX, nextY));
+                    grid[nextX][nextY] = '0';
                 }
-                if (grid[nextX][nextY] != '1') {
-                    continue;
-                }
-                queue.offer(new Coordinate(nextX, nextY));
-                grid[nextX][nextY] = '0';
             }
         }
     }
-    private boolean isValid(char[][] grid, int x, int y) {
+    private boolean inBound(int x, int y, char[][] grid) {
         return 0 <= x && x < grid.length && 0 <= y && y < grid[0].length;
     }
+    
 }
