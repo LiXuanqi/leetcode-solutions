bfba01e875375ece14b9d281817af44a5161de92
lixuanqi <lixuanqi1995@gmail.com>
2019-01-17 15:48:05 -0800

    784

--- java/784-letter-case-permutation.java
+++ java/784-letter-case-permutation.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<String> letterCasePermutation(String S) {
         List<String> ans = new ArrayList<>();
-        if (S == null || S.length() == 0) {
-            ans.add("");
+        if (S == null) {
             return ans;
         }
         dfs(S, 0, new StringBuilder(), ans);
         return ans;
     }
-    private void dfs(String S, int index, StringBuilder sb, List<String> ans) {
-        if (index >= S.length()) {
-            if (sb.length() == S.length()) {
-                ans.add(sb.toString());
-            }
+    private void dfs(String str, int index, StringBuilder sb, List<String> ans) {
+        if (index >= str.length()) {
+            ans.add(sb.toString());
             return;
         }
-        if (Character.isLetter(S.charAt(index))) {
-            sb.append(Character.toUpperCase(S.charAt(index)));
-            dfs(S, index + 1, sb, ans);
-            sb.deleteCharAt(sb.length() - 1);
-            sb.append(Character.toLowerCase(S.charAt(index)));
-            dfs(S, index + 1, sb, ans);
+        char c = str.charAt(index);
+        if (Character.isLetter(c)) {
+            sb.append(Character.toUpperCase(c));
+            dfs(str, index + 1, sb, ans);
+            sb.deleteCharAt(sb.length() - 1);       
+            sb.append(Character.toLowerCase(c));
+            dfs(str, index + 1, sb, ans);
             sb.deleteCharAt(sb.length() - 1);
         } else {
-            sb.append(S.charAt(index));
-            dfs(S, index + 1, sb, ans);
+            sb.append(c);
+            dfs(str, index + 1, sb, ans);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
