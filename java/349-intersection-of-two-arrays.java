dff098ee8ceb558ca4ca5170dc24c7c3b862c370
lixuanqi <lixuanqi1995@gmail.com>
2019-01-16 17:10:04 -0800

    349

--- java/349-intersection-of-two-arrays.java
+++ java/349-intersection-of-two-arrays.java
@@ -1,13 +1,10 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        if (nums1 == null || nums2 == null) {
-            return null;
-        }
         Set<Integer> set = new HashSet<>();
+        Set<Integer> ans = new HashSet<>();
         for (int num : nums1) {
             set.add(num);
         }
-        Set<Integer> ans = new HashSet<>();
         for (int num : nums2) {
             if (set.contains(num)) {
                 ans.add(num);
