5f5b6bf63fa28fbce2002b190efa50ad7b620dd4
lixuanqi <lixuanqi1995@gmail.com>
2019-01-17 15:27:45 -0800

    21

--- java/21-merge-two-sorted-lists.java
+++ java/21-merge-two-sorted-lists.java
@@ -8,38 +8,8 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null){
-            return l2;
-        }
-        if(l2 == null){
-            return l1;
-        }
-        
-        if(l1.val < l2.val){
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
-    }
-}
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
         }
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
@@ -53,20 +23,7 @@ class Solution {
             }
             curr = curr.next;
         }
-        while (l1 != null) {
-            curr.next = l1;
-            l1 = l1.next;
-            curr = curr.next;
-        }
-        while (l2 != null) {
-            curr.next = l2;
-            l2 = l2.next;
-            curr = curr.next;
-        }
+        curr.next = l1 == null ? l2 : l1;
         return dummy.next;
     }
 }
-
-// 1   ->     2    ->     4
-
-// 1   ->     3    ->     4
