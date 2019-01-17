706a15e0213c31905a5dcef37e103a1403505b4f
lixuanqi <lixuanqi1995@gmail.com>
2019-01-17 15:57:09 -0800

    50

--- java/50-pow.java
+++ java/50-pow.java
@@ -7,11 +7,11 @@ class Solution {
         }
     }
     private double pow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
         if (n == 1) {
             return x;
+        }
+        if (n == 0) {
+            return 1;
         }
         double product = pow(x, n / 2);
         if (n % 2 == 0) {
