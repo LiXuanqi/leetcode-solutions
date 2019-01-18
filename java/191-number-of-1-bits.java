c894929639c7fde5c88c104273244a5cd7e3eac8
lixuanqi <lixuanqi1995@gmail.com>
2019-01-18 10:43:36 -0800

    191

--- java/191-number-of-1-bits.java
+++ java/191-number-of-1-bits.java
@@ -9,3 +9,16 @@ public class Solution {
         return count;
     }
 }
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = n; i != 0; i = i >>> 1) {
+            if ((i & 1) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
