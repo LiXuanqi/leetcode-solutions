eefb65ad797cde7380e0140f2bb11c0d705a21bf
lixuanqi <lixuanqi1995@gmail.com>
2019-01-19 15:50:32 -0800

    88

--- java/88-merge-sorted-array.java
+++ java/88-merge-sorted-array.java
@@ -1,20 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int pointer1 = m - 1;
-        int pointer2 = n - 1;
         int curr = nums1.length - 1;
-        while (pointer1 >= 0 && pointer2 >= 0) {
-            if (nums1[pointer1] > nums2[pointer2]) {
-                nums1[curr--] = nums1[pointer1--];
+        int p1 = m - 1;
+        int p2 = n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] < nums2[p2]) {
+                nums1[curr--] = nums2[p2--];
             } else {
-                nums1[curr--] = nums2[pointer2--];
+                nums1[curr--] = nums1[p1--];
             }
         }
-        while (pointer1 >= 0) {
-            nums1[curr--] = nums1[pointer1--];
+        while (p1 >= 0) {
+            nums1[curr--] = nums1[p1--];
+            
         }
-        while (pointer2 >= 0) {
-            nums1[curr--] = nums2[pointer2--];
+        while (p2 >= 0) {
+            nums1[curr--] = nums2[p2--]; 
         }
     }
 }
