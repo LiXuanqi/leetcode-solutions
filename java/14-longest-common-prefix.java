110098d2dbdbc5d4c304110d1e8bf03d36dc0570
lixuanqi <lixuanqi1995@gmail.com>
2019-01-19 15:01:56 -0800

    14

--- java/14-longest-common-prefix.java
+++ java/14-longest-common-prefix.java
@@ -3,18 +3,18 @@ class Solution {
         if (strs == null || strs.length == 0) {
             return "";
         }
-        String ans = null;
-        for (String str : strs) {
-            ans = findCommon(ans, str);
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            ans = findCommon(ans, strs[i]);
         }
         return ans;
     }
     private String findCommon(String str1, String str2) {
-        if (str1 == null) {
-            return str2;
-        }
         int i = 0;
-        while (i < str1.length() && i < str2.length() && str1.charAt(i) == str2.charAt(i)) {
+        while (i < str1.length() && i < str2.length()) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                break;
+            }
             i++;
         }
         return str1.substring(0, i);
