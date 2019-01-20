05f63f0788f5781c06e8d306cb9b7ed7ec9fcfa8
lixuanqi <lixuanqi1995@gmail.com>
2019-01-20 14:22:26 -0800

    46

--- java/46-permutaions.java
+++ java/46-permutaions.java
@@ -7,18 +7,23 @@ class Solution {
         dfs(nums, new boolean[nums.length], new ArrayList<>(), ans);
         return ans;
     }
-    private void dfs(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> ans) {
+    private void dfs(
+        int[] nums,
+        boolean[] visited,
+        List<Integer> temp,
+        List<List<Integer>> ans
+    ) {
         if (temp.size() == nums.length) {
             ans.add(new ArrayList<>(temp));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (!used[i]) {
+            if (!visited[i]) {
                 temp.add(nums[i]);
-                used[i] = true;
-                dfs(nums, used, temp, ans);
+                visited[i] = true;
+                dfs(nums, visited, temp, ans);
+                visited[i] = false;
                 temp.remove(temp.size() - 1);
-                used[i] = false;
             }
         }
     }
