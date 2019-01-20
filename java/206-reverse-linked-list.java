19a294602142ebae0da6feaeaeea6ea336a83248
lixuanqi <lixuanqi1995@gmail.com>
2019-01-19 16:24:54 -0800

    206

--- java/206-reverse-linked-list.java
+++ java/206-reverse-linked-list.java
@@ -8,9 +8,6 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
@@ -22,21 +19,3 @@ class Solution {
         return prev;
     }
 }
-
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
-    }
-}
-
-//   1  ->  (  2   ->    3  ->    4  ->    5 )
-// curr       next
-
-//   1  ->  (  2   ->    3  ->    4  ->    5 )
-//                                        curr       next
