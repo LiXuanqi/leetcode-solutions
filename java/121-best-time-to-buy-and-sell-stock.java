9f9118baf39ae2017edc1cc59ed02d276046dd0c
lixuanqi <lixuanqi1995@gmail.com>
2019-01-19 20:14:55 -0800

    121

--- java/121-best-time-to-buy-and-sell-stock.java
+++ java/121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-            return 0;
-        }
-        int lowest = Integer.MAX_VALUE;
         int ans = 0;
+        int lowest = Integer.MAX_VALUE;
         for (int price : prices) {
             if (price < lowest) {
                 lowest = price;
             } else {
-                int profit = price - lowest;
-                ans = Math.max(ans, profit);
+                int diff = price - lowest;
+                ans = Math.max(ans, diff);
             }
         }
         return ans;
