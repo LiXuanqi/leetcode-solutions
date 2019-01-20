2caa715dc0f2f7faa8d173522556fa4e5d1a60ee
lixuanqi <lixuanqi1995@gmail.com>
2019-01-19 23:45:55 -0800

    309

--- java/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ java/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,25 +3,22 @@ class Solution {
         if (prices == null || prices.length == 0) {
             return 0;
         }
-        long[][] dp = new long[prices.length][4];
-        // 0: unhold, nothing.
-        // 1: unhold, sell.
-        // 2: hold, nothing.
-        // 3: hold, buy.
-        dp[0][0] = 0;
+        // 0: hold
+        // 1: not hold, sell
+        // 2: not hold, not sell
+        long[][] dp = new long[prices.length][3];
+        dp[0][0] = -prices[0];
         dp[0][1] = Integer.MIN_VALUE;
-        dp[0][2] = Integer.MIN_VALUE;
-        dp[0][3] = -prices[0];
-        int maxProfit = 0;
-        for (int i = 1; i < dp.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][1] = Math.max(dp[i - 1][2] + prices[i], dp[i - 1][3] + prices[i]);
-            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][3]);
-            dp[i][3] = dp[i - 1][0] - prices[i];
-            
-            maxProfit = Math.max(maxProfit, (int) dp[i][0]);
-            maxProfit = Math.max(maxProfit, (int) dp[i][1]);
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1]);
         }
-        return maxProfit;
+        int ans = 0;
+        for (int i = 0; i < dp.length; i++) {
+            ans = Math.max(ans, (int) dp[i][1]);
+            ans = Math.max(ans, (int) dp[i][2]);
+        }
+        return ans;
     }
 }
