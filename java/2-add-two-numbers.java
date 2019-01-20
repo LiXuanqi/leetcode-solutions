b1671186d274eeaf873ad009fab3ede7b610e05d
lixuanqi <lixuanqi1995@gmail.com>
2019-01-19 16:21:02 -0800

    2

--- java/2-add-two-numbers.java
+++ java/2-add-two-numbers.java
@@ -8,9 +8,12 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        int rest = 0;
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
-        int rest = 0;
         while (l1 != null || l2 != null) {
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
