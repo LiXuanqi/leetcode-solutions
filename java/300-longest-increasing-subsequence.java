8fd783fda2324bf49128d142219dda0790c9e1bf
lixuanqi <lixuanqi1995@gmail.com>
2019-01-21 12:13:18 -0800

    300

--- java/300-longest-increasing-subsequence.java
+++ java/300-longest-increasing-subsequence.java
@@ -19,3 +19,42 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        Arrays.fill(tails, Integer.MAX_VALUE);
+        // tails[0] = Integer.MIN_VALUE;
+        for (int num : nums) {
+            int index = findFirstLargerOrEquals(tails, num);
+            tails[index] = num;
+        }
+        int ans = tails.length - 1;
+        for (; ans >= 0; ans--) {
+            if (tails[ans] != Integer.MAX_VALUE) {
+                return ans + 1;
+            }
+        }
+        return 0;
+    }
+    private int findFirstLargerOrEquals(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+ 
+        if (nums[start] >= target) {
+            return start;
+        }
+        if (nums[end] >= target) {
+            return end;
+        }
+        return end + 1;
+    }
+}
