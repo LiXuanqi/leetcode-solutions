1bc101982c57a231fbf8ee8908d15b3d1d2c5a63
lixuanqi <lixuanqi1995@gmail.com>
2019-01-22 11:54:29 -0800

    443

--- java/443-string-compression.java
+++ java/443-string-compression.java
@@ -3,14 +3,14 @@ class Solution {
         if (chars == null || chars.length == 0) {
             return 0;
         }
-        int slow = 0;
         int anchor = 0;
+        int slow = 0;
         for (int fast = 0; fast < chars.length; fast++) {
-            if (fast == chars.length - 1 || chars[fast + 1] != chars[fast]) {
-                // time to write.
+            if (fast == chars.length - 1 || chars[fast] != chars[fast + 1]) {
+                // to write.
                 chars[slow++] = chars[anchor];
-                int length = fast - anchor + 1;
-                if (length > 1) {
+                if (fast != anchor) {
+                    int length = fast - anchor + 1;
                     for (char c : String.valueOf(length).toCharArray()) {
                         chars[slow++] = c;
                     }
