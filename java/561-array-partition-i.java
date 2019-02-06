97e032821eb5d923853bfd91f171839c4c861d61
lixuanqi <lixuanqi1995@gmail.com>
2019-02-06 18:26:21 -0500

    561

---
+++ java/561-array-partition-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            ans += nums[i];
+        }
+        return ans;
+    }
+}
