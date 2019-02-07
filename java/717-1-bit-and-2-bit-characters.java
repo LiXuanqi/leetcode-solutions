b106e1517ad1600136ff264b594c22a23c32ac8b
lixuanqi <lixuanqi1995@gmail.com>
2019-02-07 13:00:19 -0500

    717

--- java/717-1-bit-and-2-bit-characters.java
+++ java/717-1-bit-and-2-bit-characters.java
@@ -1,29 +1,9 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        if (bits == null || bits.length == 0) {
-            return false;
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] + 1;
         }
-        Set<String> set = new HashSet<>();
-        set.add("10");
-        set.add("11");
-        set.add("0");
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bits.length - 1; i++) {
-            sb.append(bits[i]);
-        }
-        return canMatch(sb.toString(), 0, set);
-    }
-    private boolean canMatch(String str, int start, Set<String> set) {
-        if (start >= str.length()) {
-            return true;
-        }
-        boolean result = false;
-        if (set.contains(str.substring(start, start + 1)) && canMatch(str, start + 1, set)) {
-            result = true;
-        }
-        if (start < str.length() - 1 && set.contains(str.substring(start, start + 2)) && canMatch(str, start + 2, set)) {
-            result = true;
-        }
-        return result;
+        return i == bits.length - 1;
     }
 }
