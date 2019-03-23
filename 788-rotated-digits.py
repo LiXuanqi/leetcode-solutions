60b3f770f7f7b72f66828b532e6f6576a840068a
lixuanqi <lixuanqi1995@gmail.com>
2019-03-22 17:27:01 -0700

    788

--- 788-rotated-digits.py
+++ 788-rotated-digits.py
@@ -1,27 +1,7 @@
-digits = {
-    '0': '0',
-    '1': '1',
-    '2': '5',
-    '5': '2',
-    '6': '9',
-    '8': '8',
-    '9': '6'
-}
-
 class Solution:
     def rotatedDigits(self, N: int) -> int:
-        count = 0
-        for num in range(N + 1):
-            if any([c not in digits for c in str(num)]):
-                continue
-            # - can rotate
-            if rotate(num) != num:
-                count += 1
-        return count
-    
-def rotate(num):
-    num = str(num)
-    ans = "".join([digits[c] for c in num])
-    return int(ans)
-    
-        
+        ans = 0
+        for i in range(1, N + 1):
+            S = str(i)
+            ans += (all([c not in '347' for c in S]) and any([c in '2569' for c in S]))
+        return ans
