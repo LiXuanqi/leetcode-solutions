3e51f01fabeacbb6f8645dc2814d3ad337570aaa
lixuanqi <lixuanqi1995@gmail.com>
2019-03-22 20:23:51 -0700

    734 with setdefault

--- 734-sentence-similarity.py
+++ 734-sentence-similarity.py
@@ -5,12 +5,8 @@ class Solution:
         # - build dict
         d = {}
         for word1, word2 in pairs:
-            l1 = d.get(word1, [])
-            l2 = d.get(word2, [])
-            l1.append(word2)
-            l2.append(word1)
-            d[word1] = l1
-            d[word2] = l2
+            d.setdefault(word1, []).append(word2)
+            d.setdefault(word2, []).append(word1)
         for i in range(len(words1)):
             if words1[i] == words2[i]:
                 continue
