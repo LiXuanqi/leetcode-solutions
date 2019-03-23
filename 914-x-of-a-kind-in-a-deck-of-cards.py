ee9d4cc56ef248b60ea52b2fe40bef4cf031cd69
lixuanqi <lixuanqi1995@gmail.com>
2019-03-23 00:57:22 -0700

    914

--- 914-x-of-a-kind-in-a-deck-of-cards.py
+++ 914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,13 +1,11 @@
 from collections import Counter
+from functools import reduce
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
         # count
         counter = Counter(deck)
-        minX = min(counter.values())
-        maxX = max(counter.values())
-        if minX <= 1:
-            return False
-        for x in range(2, minX + 1):
-            if all([value % x == 0 for value in counter.values()]):
-                return True
-        return False
+        return reduce(gcd, counter.values()) >= 2
+def gcd(num1, num2):
+    if num1 == 0:
+        return num2
+    return gcd(num2 % num1, num1)
