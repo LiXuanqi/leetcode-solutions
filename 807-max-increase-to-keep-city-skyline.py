7fc81bd0fb30948e900b092871bb0cfb4e89716a
lixuanqi <lixuanqi1995@gmail.com>
2019-03-23 17:07:05 -0700

    807

--- 807-max-increase-to-keep-city-skyline.py
+++ 807-max-increase-to-keep-city-skyline.py
@@ -1,18 +1,10 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        maxXs = [0] * len(grid)
-        maxYs = [0] * len(grid[0])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                num = grid[i][j]
-                # - update max array.
-                maxXs[i] = max(maxXs[i], num)
-                maxYs[j] = max(maxYs[j], num)
-        ans = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                maxValue = min(maxXs[i], maxYs[j])
-                if maxValue > grid[i][j]:
-                    ans += maxValue - grid[i][j]
-        return ans
+        maxXs = [max(row) for row in grid]
+        maxYs = [max(col) for col in zip(*grid)]
+        return sum(
+            min(maxXs[i], maxYs[j]) - val
+            for i, row in enumerate(grid)
+            for j, val in enumerate(row)
+        )
                 
