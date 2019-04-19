49bcf9ec524acfb8dbbc05ea00e55fad51a40615
lixuanqi <lixuanqi1995@gmail.com>
2019-04-19 15:41:09 -0400

    609

---
+++ python/609-find-duplicate-file-in-system.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        if not paths:
+            return []
+        
+        def decodePath(path, contentToPaths):
+            args = path.split(" ")
+            folderPath = args[0]
+            for i in range(1, len(args)):
+                file = args[i]
+                leftParenthese = file.index("(")
+                filename = file[:leftParenthese]
+                content = file[leftParenthese + 1 : -1]
+                if content not in contentToPaths:
+                    contentToPaths[content] = []
+                contentToPaths[content].append(folderPath + "/" + filename)
+        
+        contentToPaths = {}
+        for path in paths:
+            decodePath(path, contentToPaths)
+        return list([value for value in contentToPaths.values() if len(value) > 1])
