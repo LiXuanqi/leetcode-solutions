487f46852b2d001ed0354a9193f94856d580ca80
lixuanqi <lixuanqi1995@gmail.com>
2019-04-24 00:17:26 -0400

    45

--- python/45-jump-game-ii.py
+++ python/45-jump-game-ii.py
@@ -1,27 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # - bfs
         if not nums:
             return -1
-        if len(nums) <= 1:
-            return 0
-        visited = [False] * len(nums)
-        queue = collections.deque()
-        queue.append(0)
-        visited[0] = True
-        ans = 0
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                curr = queue.popleft()
-                # - neighbbors
-                for step in reversed(range(1, nums[curr] + 1)):
-                    next = curr + step
-                    if next >= len(nums) - 1:
-                        return ans + 1
-                    if not visited[next]:
-                        queue.append(next)
-                        visited[next] = True
-            ans += 1
-        return ans
-        
+        jump = 0
+        currFarest = 0
+        currEnd = 0
+        for i in range(len(nums) - 1):
+            currFarest = max(currFarest, i + nums[i])
+            if i == currEnd:
+                jump += 1
+                currEnd = currFarest
+        return jump
