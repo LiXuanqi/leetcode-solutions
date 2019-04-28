ae80b428e1a90ff2c5ae3bc8b437081e0e3a9b9e
lixuanqi <lixuanqi1995@gmail.com>
2019-04-28 18:27:22 -0400

    binary search.

--- python/778-swim-in-rising-water.py
+++ python/778-swim-in-rising-water.py
@@ -6,22 +6,41 @@ class Solution:
         def inBound(x, y):    
             return 0 <= x < n and 0 <= y < n
         
+        def canReach(height):
+            if grid[0][0] > height:
+                return False
+            seen = {(0, 0)}
+            queue = collections.deque()
+            queue.append((0, 0))
+            while queue:
+                x, y = queue.popleft()
+                if x == n - 1 and y == n - 1:
+                    return True
+                for nextX, nextY in ((x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)):
+                    if inBound(nextX, nextY) and (nextX, nextY) not in seen and grid[nextX][nextY] <= height:
+                        queue.append((nextX, nextY))
+                        seen.add((nextX, nextY))
+            return False;
+            
         n = len(grid)
-        Point = collections.namedtuple('Point', 'cost, x, y')
-        seen = set()
-        pq = []
-        heapq.heappush(pq, Point(grid[0][0], 0, 0))
-        seen.add((0, 0))
-        while pq:
-            cost, x, y = heapq.heappop(pq)
-            if x == n - 1 and y == n - 1:
-                return cost
-            # 4 directions
-            for nextX, nextY in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                if inBound(nextX, nextY) and (nextX, nextY) not in seen:
-                    heapq.heappush(pq, Point(max(cost, grid[nextX][nextY]), nextX, nextY))
-                    seen.add((nextX, nextY))
+        left = 0
+        right = max(max(row) for row in grid)
+        
+        
+        
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if canReach(mid):
+                right = mid
+            else:
+                left = mid
+        if canReach(left):
+            return left
+        if canReach(right):
+            return right
         return -1
+    
+            
             
             
         
