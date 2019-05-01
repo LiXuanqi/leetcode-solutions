4850485c7490094d992405a0b3441856b4f54acc
lixuanqi <lixuanqi1995@gmail.com>
2019-05-01 16:55:23 -0400

    264

--- python/264-ugly-number-ii.py
+++ python/264-ugly-number-ii.py
@@ -1,16 +1,19 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        if n <= 0:
-            return 0
-        pq = [1]
-        seen = {1}
-        while n > 1:
-            num = heapq.heappop(pq)
-            n -= 1
-            for factor in (2, 3, 5):
-                next = num * factor
-                if next not in seen:
-                    heapq.heappush(pq, next)
-                    seen.add(next)
-        return heapq.heappop(pq)
+        if n <= 1:
+            return n
+        uglys = [1]
+        index2 = 0
+        index3 = 0
+        index5 = 0
+        for _ in range(n):
+            minVal = min(uglys[index2] * 2, uglys[index3] * 3, uglys[index5] * 5)
+            uglys.append(minVal)
+            if uglys[index2] * 2 == minVal:
+                index2 += 1
+            if uglys[index3] * 3 == minVal:
+                index3 += 1
+            if uglys[index5] * 5 == minVal:
+                index5 += 1
+        return uglys[n - 1]
         
