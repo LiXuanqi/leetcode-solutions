741993c4e74c8fe8c1fa01953e48c0a0e8d16885
lixuanqi <lixuanqi1995@gmail.com>
2019-05-01 17:27:26 -0400

    250

--- python/250-count-univalue-subtrees.py
+++ python/250-count-univalue-subtrees.py
@@ -8,20 +8,25 @@
 class Solution:
     
     def countUnivalSubtrees(self, root: TreeNode) -> int:
-        
         if root is None:
             return 0
-        if root.left is None and root.right is None:
-            return 1
-        return self.countUnivalSubtrees(root.left) + self.countUnivalSubtrees(root.right) + (1 if isUnivalSubtree(root) else 0)
+        self.ans = 0
+        self.isUnivalSubtree(root)
+        return self.ans
         
-def isUnivalSubtree(root):
-    if root is None:
-        return True
-    if root.left is None and root.right is None:
-        return True
-    if root.left is None:
-        return isUnivalSubtree(root.right) and root.right.val == root.val
-    if root.right is None:
-        return isUnivalSubtree(root.left) and root.left.val == root.val
-    return isUnivalSubtree(root.left) and isUnivalSubtree(root.right) and root.left.val == root.right.val == root.val
+    def isUnivalSubtree(self, root):
+        if root.left is None and root.right is None:
+            self.ans += 1
+            return True
+        if root.left is None:
+            isUni = self.isUnivalSubtree(root.right) and root.right.val == root.val
+            self.ans += isUni
+            return isUni
+        if root.right is None:
+            isUni = self.isUnivalSubtree(root.left) and root.left.val == root.val
+            self.ans += isUni
+            return isUni
+        # - 如果用if,短路会造成遍历不完所有subtrees.
+        isUni = all([self.isUnivalSubtree(root.left), self.isUnivalSubtree(root.right), root.left.val == root.right.val == root.val])
+        self.ans += isUni
+        return isUni
