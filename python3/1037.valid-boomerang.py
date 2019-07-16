b779b38a57fe590921e039b38d737c23d0622ecc
LiXuanqi <lixuanqi1995@gmail.com>
2019-07-15 23:26:51 -0700

    1037

--- python3/1037.valid-boomerang.py
+++ python3/1037.valid-boomerang.py
@@ -5,19 +5,8 @@
 #
 class Solution:
     def isBoomerang(self, points: List[List[int]]) -> bool:
-        if not points or len(points) != 3:
-            return False
-        pointSet = set()
-        for point in points:
-            pointSet.add(tuple(point))
-        point1, point2, point3 = points
-        if point1[0] == point2[0] == point3[0] or point1[1] == point2[1] == point3[1]:
-            return False
-        if len(pointSet) != 3:
-            return False
-        def getSlope(point1, point2):
-            if point2[0] - point1[0] == 0:
-                return None
-            return (point2[1] - point1[1]) / (point2[0] - point1[0])
-
-        return getSlope(point1, point2) != getSlope(point2, point3)
+        a, b, c = points
+        xa, ya = a
+        xb, yb = b
+        xc, yc = c
+        return (yb - ya) * (xc - xa) != (xb - xa) * (yc - ya)
