990451868b748cb35a002b19a30a12da4935e1c7
LiXuanqi <lixuanqi1995@gmail.com>
2021-04-12 23:06:54 -0700

    1254 add dfs

--- python3/1254.number-of-clesed-islands.py
+++ python3/1254.number-of-clesed-islands.py
@@ -11,24 +11,24 @@ class Solution:
         # Fill the bound lands.
         for i in range(n):
             if grid[i][0] == 0:
-                mark_connected_lands_as_visited(i, 0, grid)
+                mark_connected_lands_as_visited_dfs(i, 0, grid)
             if grid[i][m - 1] == 0:
-                mark_connected_lands_as_visited(i, m - 1, grid)
+                mark_connected_lands_as_visited_dfs(i, m - 1, grid)
         for i in range(m):
             if grid[0][i] == 0:
-                mark_connected_lands_as_visited(0, i, grid)
+                mark_connected_lands_as_visited_dfs(0, i, grid)
             if grid[n - 1][i] == 0:
-                mark_connected_lands_as_visited(n - 1, i, grid)
+                mark_connected_lands_as_visited_dfs(n - 1, i, grid)
         
         ans = 0
         for x in range(1, n - 1):
             for y in range(1, m - 1):
                 if grid[x][y] == 0:
-                    mark_connected_lands_as_visited(x, y, grid)
+                    mark_connected_lands_as_visited_dfs(x, y, grid)
                     ans += 1
         return ans
         
-def mark_connected_lands_as_visited(x, y, grid):
+def mark_connected_lands_as_visited_bfs(x, y, grid):
     queue = collections.deque()
     queue.append((x, y))
     while queue:
@@ -38,6 +38,14 @@ def mark_connected_lands_as_visited(x, y, grid):
             if in_bound(next_x, next_y, grid) and grid[next_x][next_y] == 0:
                 queue.append((next_x, next_y))
                 grid[next_x][next_y] = 1
+
+def mark_connected_lands_as_visited_dfs(x, y, grid):
     
+    grid[x][y] = 1
+    for diff_x, diff_y in DIRECTIONS:
+        next_x, next_y = x + diff_x, y + diff_y
+        if in_bound(next_x, next_y, grid) and grid[next_x][next_y] == 0:
+            mark_connected_lands_as_visited_dfs(x + diff_x, y + diff_y, grid)
+            
 def in_bound(x, y, grid):
     return 0 <= x < len(grid) and 0 <= y < len(grid[0])
