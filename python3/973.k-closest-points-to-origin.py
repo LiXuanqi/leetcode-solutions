a1f45af0c349c7939207a0dfb611f2f5a2ebdc5b
1_x7 <lixuanqi1995@gmail.com>
2021-04-27 00:04:04 -0700

    Update 973.k-closest-points-to-origin.py

--- python3/973.k-closest-points-to-origin.py
+++ python3/973.k-closest-points-to-origin.py
@@ -1,3 +1,40 @@
+# solution: quick select 
+# Time: O(n)  faster than 53.12%
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points = [(distance_to_origin(point), point) for point in points]
+        quick_select(points, 0, len(points) - 1, k)
+        return [point[1] for point in points[:k]]
+
+def quick_select(points, start, end, k):
+    
+    if start >= end:
+        return
+    
+    left = start
+    right = end
+    mid = (left + right) // 2
+    pivot = points[mid][0]
+    while left <= right:
+        while left <= right and points[left][0] < pivot:
+            left += 1
+        
+        while left <= right and points[right][0] > pivot:
+            right -= 1
+            
+        # swap
+        if left <= right:
+            temp = points[left]
+            points[left] = points[right]
+            points[right] = temp
+            left += 1
+            right -= 1
+        # if left = right before swap, there is one element between right and left pointer.
+    
+    if right >= k:
+        quick_select(points, start, right, k)
+    elif k >= left:
+        quick_select(points, left, end, k)
         
 # Solution: maintain the max-heap with size k
 # Time: O(nlogk)  faster than 46.58%
