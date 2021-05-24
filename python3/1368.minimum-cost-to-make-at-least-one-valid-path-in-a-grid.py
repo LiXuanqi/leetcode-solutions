0d954d61a50019aa98711ed709834559a4876a62
1_x7 <lixuanqi1995@gmail.com>
2021-05-23 22:49:24 -0700

    Update 1368.minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py

--- python3/1368.minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ python3/1368.minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -5,33 +5,37 @@ DIRECTIONS = (
     (1, 0),
     (-1, 0)
 )
+
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
         start = (0, 0)
         end = (n - 1, m - 1)
         
-       
-        heap = [(0, start)]
         costs = {
             start: 0
         }
+
+        deque = collections.deque()
+
+        deque.append((0, start))
+      
+
+        while deque:
+            curr_cost, curr = deque.popleft()
         
-        while heap:
-            curr_cost, curr = heapq.heappop(heap)
-            # print(curr_cost, curr)
+            curr_x, curr_y = curr
+          
             if curr == end:
                 return curr_cost
-            curr_x, curr_y = curr
             for direction_index, diff in enumerate(DIRECTIONS):
                 if direction_index == 0:
                     continue
                 diff_x, diff_y = diff
-                next_x = diff_x + curr_x
-                next_y = diff_y + curr_y
-                
+                next_x = curr_x + diff_x
+                next_y = curr_y + diff_y
                 next_point = (next_x, next_y)
-                next_cost = curr_cost + int(direction_index != grid[curr_x][curr_y])
+                next_cost = curr_cost + int(grid[curr_x][curr_y] != direction_index)
                 
                 if not in_bound(next_x, next_y, n, m) or (
                     next_point in costs and
@@ -39,13 +43,16 @@ class Solution:
                 ):
                     continue
                     
-                heapq.heappush(heap, (next_cost, next_point))
-                costs[next_point] = next_cost
-                
+                if grid[curr_x][curr_y] == direction_index:
+                    deque.appendleft((next_cost, next_point))
+                else:
+                    deque.append((next_cost, next_point)) 
                     
+                costs[next_point] = next_cost
+
         return -1
-                   
+
 def in_bound(x, y, n, m):
     return 0 <= x < n and 0 <= y < m
-        
+    
         
