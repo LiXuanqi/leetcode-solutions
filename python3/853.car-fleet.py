066c20a2e78b3a391ef05b5e261c61bb8178ab03
1_x7 <lixuanqi1995@gmail.com>
2021-05-31 23:23:25 -0700

    Update 853.car-fleet.py

--- python3/853.car-fleet.py
+++ python3/853.car-fleet.py
@@ -1,16 +1,27 @@
 class Solution:
-    def carFleet(self, target: int, positions: List[int], speeds: List[int]) -> int:
-        cars = sorted(zip(positions, speeds))
-        times = [(target - position) / speed for position, speed in cars]
-        ans = 0
-        while len(times) > 1:
-            rightmost_car_time = times.pop()
-            if rightmost_car_time < times[-1]:
-                ans += 1
-            else:
-                times[-1] = rightmost_car_time
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         
-            
-        return ans + (1 if times else 0)
-            
+        cars = list(zip(position, speed))
+        cars.sort()
         
+        # iterate from tail
+        stack = []
+        
+        i = len(cars) - 1
+        while i >= 0:
+            curr_car = cars[i]
+            
+            if not stack or not will_collision(curr_car, stack[-1], target):
+                stack.append(curr_car)
+            
+            i -= 1
+            
+        return len(stack)
+    
+def will_collision(first_car, second_car, total_distance):
+    if first_car[1] <= second_car[1]:
+        return False
+    possible_collision_time = (second_car[0] - first_car[0]) / (first_car[1] - second_car[1])
+    second_arrive_time = (total_distance - second_car[0]) / second_car[1]
+    return second_arrive_time >= possible_collision_time
+            
