11fe5f4883cda7b15e809ca9e5b787dc039b5f48
1_x7 <lixuanqi1995@gmail.com>
2021-06-25 01:03:07 -0700

    Create 364.nested-list-weight-sum-ii.py

---
+++ python3/364.nested-list-weight-sum-ii.py
@@ -0,0 +1,59 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        weighted = 0
+        unweighted = 0
+        
+        while nestedList:
+            next_level = []
+            for nested_integer in nestedList:
+                if nested_integer.isInteger():
+                    unweighted += nested_integer.getInteger()
+                else:
+                    next_level.extend(nested_integer.getList())
+            nestedList = next_level
+            weighted += unweighted
+        return weighted
+        
